af76697a4e40d1466977d2ca5f988ab8ed1d0249
Daniel Gutierrez <Dgutie299@west-mec.local>
2021-08-31 17:06:14 -0700

    finished gallery styles, aboutColumn styles, and footer

--- rvproject/src/components/Footer.jsx
+++ rvproject/src/components/Footer.jsx
@@ -3,7 +3,12 @@ import React, { useState } from "react";
 function Footer() {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <h4>Social Links</h4>
+      <h4>Briggs RV Repair LLC</h4>
+      <h5>24710 W Dove Trail</h5>
+      <h5>Buckeye, AZ 85326</h5>
+      <h5>+1(623)4745069</h5>
+      <p>Copyright © 2021 Briggs RV Repair LLC - All Rights Reserved.</p>
     </div>
   );
 }
--- rvproject/src/components/Gallery.jsx
+++ rvproject/src/components/Gallery.jsx
@@ -3,29 +3,49 @@ import React, { useState } from "react";
 const Gallery = () => {
   return (
     <div className="gallery-container">
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
-
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
-
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
-
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
-
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
+      </div>
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
+      </div>
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
       
-      <div class="gallery">
-        <img src="https://picsum.photos/400/400" alt="galleryimg" />
+      <div className="responsive">
+        <div class="gallery">
+          <img src="https://picsum.photos/400/400" alt="galleryimg" />
+        </div>
       </div>
+
+      
     </div>
   );
 };
